75c6006e8c200c5f5b11e8a84b11ccf4307e3dbd
Fanny Gautier <fannygautier92@gmail.com>
2024-11-29 08:33:33 +0100

    fix: lint

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   const { user, loading, fetchUserData } = useUserHook();
   useEffect(() => {
     fetchUserData();
-  }, []);
+  }, [fetchUserData]);
 
   if (loading) {
     return (
--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -2,8 +2,11 @@ import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
-export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  // Add at least one additional prop to avoid the empty interface warning
+  // This is only needed if you actually need to extend the interface
+  wrapperClassName?: string;
+}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
