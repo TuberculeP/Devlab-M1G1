0cc73a9a64dfa67bdb96e94c14985935fda9bd95
Héloïse Le Lez <lelez.heloise@gmail.com>
2024-11-29 09:33:52 +0100

    FIX: form hook and hearder links

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -3,19 +3,25 @@
 import Link from "next/link";
 
 export const Footer = () => {
-
-
-    return (
-        <>
-            <section className="footer-container">
-                <div className="footer-logo" />
-                <div className="footer-links-container">
-                    <Link href={"#"} className="footer-link">Plan du site</Link>
-                    <Link href={"#"} className="footer-link">Points de Collecte</Link>
-                    <Link href={"#"} className="footer-link">Assistance numérique</Link>
-                    <Link href={"#"} className="footer-link">FAQ</Link>
-                </div>
-            </section>
-        </>
-    )
-}
+  return (
+    <>
+      <section className="footer-container">
+        <div className="footer-logo" />
+        <div className="footer-links-container">
+          <Link href={"#"} className="footer-link">
+            Plan du site
+          </Link>
+          <Link href={"#"} className="footer-link">
+            Points de Collecte
+          </Link>
+          <Link href={"#"} className="footer-link">
+            Assistance numérique
+          </Link>
+          <Link href={"/faq"} className="footer-link">
+            FAQ
+          </Link>
+        </div>
+      </section>
+    </>
+  );
+};
--- components/header/header.tsx
+++ components/header/header.tsx
@@ -4,33 +4,57 @@ import Link from "next/link";
 import { useState } from "react";
 
 export const Header = () => {
+  const [headerEnabled, setHeaderEnabled] = useState<boolean>(false);
 
-    const [headerEnabled, setHeaderEnabled] = useState<boolean>(false)
-
-    return (
-        <>
-            <div className={headerEnabled ? "burger-menu-enabled" : "burger-menu-disabled"}>
-                <div className="burger-menu-container">
-                    <div className="cross-burger-menu" onClick={() => setHeaderEnabled(!headerEnabled)} />
-                    <div className="links-container-burger-menu">
-                        <Link href={"#"} className="burger-menu-link">Points d&apos;achat</Link>
-                        <Link href={"#"} className="burger-menu-link">Points de Collecte</Link>
-                        <Link href={"#"} className="burger-menu-link">Assistance numérique</Link>
-                        <Link href={"#"} className="burger-menu-link">FAQ</Link>
-                    </div>
-                </div>
-            </div>
-            <div className="header-container">
-                <div className="header-logo" />
-                <div className="links-container">
-                    <Link href={"#"} className="header-link">Points d&apos;achat</Link>
-                    <Link href={"#"} className="header-link">Points de Collecte</Link>
-                    <Link href={"#"} className="header-link">Assistance numérique</Link>
-                    <Link href={"#"} className="header-link">FAQ</Link>
-                    <div className="burger-menu-btn" onClick={() => setHeaderEnabled(!headerEnabled)} />
-                </div>
-            </div>
-
-        </>
-    )
-}
+  return (
+    <>
+      <div
+        className={
+          headerEnabled ? "burger-menu-enabled" : "burger-menu-disabled"
+        }
+      >
+        <div className="burger-menu-container">
+          <div
+            className="cross-burger-menu"
+            onClick={() => setHeaderEnabled(!headerEnabled)}
+          />
+          <div className="links-container-burger-menu">
+            <Link href={"#"} className="burger-menu-link">
+              Points d&apos;achat
+            </Link>
+            <Link href={"#"} className="burger-menu-link">
+              Points de Collecte
+            </Link>
+            <Link href={"#"} className="burger-menu-link">
+              Assistance numérique
+            </Link>
+            <Link href={"/faq"} className="burger-menu-link">
+              FAQ
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className="header-container">
+        <div className="header-logo" />
+        <div className="links-container">
+          <Link href={"#"} className="header-link">
+            Points d&apos;achat
+          </Link>
+          <Link href={"#"} className="header-link">
+            Points de Collecte
+          </Link>
+          <Link href={"#"} className="header-link">
+            Assistance numérique
+          </Link>
+          <Link href={"/faq"} className="header-link">
+            FAQ
+          </Link>
+          <div
+            className="burger-menu-btn"
+            onClick={() => setHeaderEnabled(!headerEnabled)}
+          />
+        </div>
+      </div>
+    </>
+  );
+};
