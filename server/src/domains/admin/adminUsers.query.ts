fb0123d03f800a52b3ae3beee8cc1c9ee1183f91
Félix Laviéville <strurphe@MacBook-Pro-de-Felix.local>
2025-01-06 16:48:17 +0100

    feat: complete course for password change

--- server/src/domains/admin/adminUsers.query.ts
+++ server/src/domains/admin/adminUsers.query.ts
@@ -1,1 +1,3 @@
 export const GET_ADMIN_USERS_QUERY = `SELECT * FROM users`;
+
+export const UPDATE_ADMIN_PASSWORD_QUERY = `UPDATE users SET password = $1 WHERE id = $2`;
--- server/src/domains/admin/adminUsers.service.ts
+++ server/src/domains/admin/adminUsers.service.ts
@@ -1,10 +1,22 @@
 import { pg } from "../../config/db.config";
 import { PgUser } from "../../../../types/users";
-import { GET_ADMIN_USERS_QUERY } from "./adminUsers.query";
+import {
+  GET_ADMIN_USERS_QUERY,
+  UPDATE_ADMIN_PASSWORD_QUERY,
+} from "./adminUsers.query";
+import { createHash } from "crypto";
 
 async function getAdminUsers() {
   const { rows } = await pg.query<PgUser>(GET_ADMIN_USERS_QUERY);
   return rows;
 }
 
-export { getAdminUsers };
+async function updatePassword(user_id: string, password: string) {
+  const encryptedPassword = createHash("sha256").update(password).digest("hex");
+  await pg.query<PgUser>(UPDATE_ADMIN_PASSWORD_QUERY, [
+    encryptedPassword,
+    user_id,
+  ]);
+}
+
+export { getAdminUsers, updatePassword };
