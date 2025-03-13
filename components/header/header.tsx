7da97b2555b3d618d82da194a8941f86b65cb4f5
Félix Laviéville <lavieville.felix@gmail.com>
2025-03-13 19:12:38 +0100

    fix: correcting bugs

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,12 +1,14 @@
 "use client";
 
 import Link from "next/link";
-<<<<<<< HEAD
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { Button } from "@/components/ui/button";
+import { DarkAndLightBtn } from "../ui/darkAndLightBtn/darkAndLightBtn";
+import { LightAndDarkModeContext } from "@/context/lightAndDarkMode";
 
 export const Header = () => {
   const [headerEnabled, setHeaderEnabled] = useState<boolean>(false);
+  const { isDark } = useContext(LightAndDarkModeContext)!;
 
   return (
     <>
@@ -15,9 +17,13 @@ export const Header = () => {
           headerEnabled ? "burger-menu-enabled" : "burger-menu-disabled"
         }
       >
-        <div className="burger-menu-container">
+        <div
+          className={
+            isDark ? "burger-menu-container-dark" : "burger-menu-container"
+          }
+        >
           <div
-            className="cross-burger-menu"
+            className={isDark ? "dark-cross-burger-menu" : "cross-burger-menu"}
             onClick={() => setHeaderEnabled(!headerEnabled)}
           />
           <div className="links-container-burger-menu">
@@ -25,7 +31,12 @@ export const Header = () => {
               href={"/purchase"}
               onClick={() => setHeaderEnabled(!headerEnabled)}
             >
-              <Button className="burger-menu-link" variant="link">
+              <Button
+                className={
+                  isDark ? "burger-menu-link white" : "burger-menu-link"
+                }
+                variant="link"
+              >
                 Points d&apos;achat
               </Button>
             </Link>
@@ -33,7 +44,12 @@ export const Header = () => {
               href={"/collect"}
               onClick={() => setHeaderEnabled(!headerEnabled)}
             >
-              <Button className="burger-menu-link" variant="link">
+              <Button
+                className={
+                  isDark ? "burger-menu-link white" : "burger-menu-link"
+                }
+                variant="link"
+              >
                 Points de Collecte
               </Button>
             </Link>
@@ -41,12 +57,22 @@ export const Header = () => {
               href={"/repairer"}
               onClick={() => setHeaderEnabled(!headerEnabled)}
             >
-              <Button className="burger-menu-link" variant="link">
+              <Button
+                className={
+                  isDark ? "burger-menu-link white" : "burger-menu-link"
+                }
+                variant="link"
+              >
                 Trouver un réparateur
               </Button>
             </Link>
             <Link href={"#"} onClick={() => setHeaderEnabled(!headerEnabled)}>
-              <Button className="burger-menu-link" variant="link">
+              <Button
+                className={
+                  isDark ? "burger-menu-link white" : "burger-menu-link"
+                }
+                variant="link"
+              >
                 Assistance numérique
               </Button>
             </Link>
@@ -54,35 +80,54 @@ export const Header = () => {
               href={"/faq"}
               onClick={() => setHeaderEnabled(!headerEnabled)}
             >
-              <Button className="burger-menu-link" variant="link">
+              <Button
+                className={
+                  isDark ? "burger-menu-link white" : "burger-menu-link"
+                }
+                variant="link"
+              >
                 FAQ
               </Button>
             </Link>
+            <DarkAndLightBtn />
           </div>
         </div>
       </div>
-      <div className="header-container">
-        <Link href={"/"}>
-          <div className="header-logo" />
+      <div className={isDark ? "header-container-dark" : "header-container"}>
+        <Link href={"/"} className="logo-link">
+          <div className={isDark ? "header-logo-dark" : "header-logo"} />
         </Link>
         <div className="links-container">
           <Link href={"/purchase"} className="header-link">
-            <Button variant="link">Points d&apos;achat</Button>
+            <Button className={isDark ? "white" : ""} variant="link">
+              Points d&apos;achat
+            </Button>
           </Link>
           <Link href={"/collect"} className="header-link">
-            <Button variant="link">Points de Collecte</Button>
+            <Button className={isDark ? "white" : ""} variant="link">
+              Points de Collecte
+            </Button>
           </Link>
           <Link href={"/repairer"} className="header-link">
-            <Button variant="link">Trouver un réparateur</Button>
+            <Button className={isDark ? "white" : ""} variant="link">
+              Trouver un réparateur
+            </Button>
           </Link>
           <Link href={"#"} className="header-link">
-            <Button variant="link">Assistance numérique</Button>
+            <Button className={isDark ? "white" : ""} variant="link">
+              Assistance numérique
+            </Button>
           </Link>
           <Link href={"/faq"} className="header-link">
-            <Button variant="link">FAQ</Button>
+            <Button className={isDark ? "white" : ""} variant="link">
+              FAQ
+            </Button>
           </Link>
+          <div id="dark-laptop-btn">
+            <DarkAndLightBtn />
+          </div>
           <div
-            className="burger-menu-btn"
+            className={isDark ? "dark-burger-menu-btn" : "burger-menu-btn"}
             onClick={() => setHeaderEnabled(!headerEnabled)}
           />
         </div>
@@ -90,77 +135,3 @@ export const Header = () => {
     </>
   );
 };
-=======
-import { useContext, useState } from "react";
-import { Button } from "@/components/ui/button"
-import { DarkAndLightBtn } from "../ui/darkAndLightBtn/darkAndLightBtn";
-import { LightAndDarkModeContext } from "@/context/lightAndDarkMode";
-
-export const Header = () => {
-    const [headerEnabled, setHeaderEnabled] = useState<boolean>(false);
-    const { isDark } = useContext(LightAndDarkModeContext)!;
-
-    return (
-        <>
-            <div className={headerEnabled ? "burger-menu-enabled" : "burger-menu-disabled"}>
-                <div className={isDark ? "burger-menu-container-dark" : "burger-menu-container"}>
-                    <div className={isDark ? "dark-cross-burger-menu" : "cross-burger-menu"} onClick={() => setHeaderEnabled(!headerEnabled)} />
-                    <div className="links-container-burger-menu">
-                        <Link href={"/purchase"} onClick={() => setHeaderEnabled(!headerEnabled)}>
-                            <Button className={isDark ? "burger-menu-link white" : "burger-menu-link"} variant="link">Points d&apos;achat
-                            </Button>
-                        </Link>
-                        <Link href={'/collect'} onClick={() => setHeaderEnabled(!headerEnabled)}>
-                            <Button className={isDark ? "burger-menu-link white" : "burger-menu-link"} variant="link">Points de Collecte
-                            </Button>
-                        </Link>
-                        <Link href={"/repairer"} onClick={() => setHeaderEnabled(!headerEnabled)}>
-                            <Button className={isDark ? "burger-menu-link white" : "burger-menu-link"} variant="link">Trouver un réparateur
-                            </Button>
-                        </Link>
-                        <Link href={"#"} onClick={() => setHeaderEnabled(!headerEnabled)}>
-                            <Button className={isDark ? "burger-menu-link white" : "burger-menu-link"} variant="link">Assistance numérique
-                            </Button>
-                        </Link>
-                        <Link href={"/faq"} onClick={() => setHeaderEnabled(!headerEnabled)}>
-                            <Button className={isDark ? "burger-menu-link white" : "burger-menu-link"} variant="link">FAQ
-                            </Button>
-                        </Link>
-                        <DarkAndLightBtn />
-                    </div>
-                </div>
-            </div>
-            <div className={isDark ? "header-container-dark" : "header-container"}>
-                <Link href={"/"} className="logo-link">
-                    <div className={isDark ? "header-logo-dark" : "header-logo"} />
-                </Link>
-                <div className="links-container">
-                    <Link href={"/purchase"} className="header-link">
-                        <Button className={isDark ? "white" : ""} variant="link">Points d&apos;achat</Button>
-                    </Link>
-                    <Link href={'/collect'} className="header-link">
-                        <Button className={isDark ? "white" : ""} variant="link">Points de Collecte</Button>
-                    </Link>
-                    <Link href={"/repairer"} className="header-link">
-                        <Button className={isDark ? "white" : ""} variant="link">Trouver un réparateur
-                        </Button>
-                    </Link>
-                    <Link href={"#"} className="header-link">
-                        <Button className={isDark ? "white" : ""} variant="link">Assistance numérique
-                        </Button>
-                    </Link>
-                    <Link href={"/faq"} className="header-link">
-                        <Button className={isDark ? "white" : ""} variant="link">FAQ
-                        </Button>
-                    </Link>
-                    <div id="dark-laptop-btn">
-                        <DarkAndLightBtn />
-                    </div>
-                    <div className={isDark ? "dark-burger-menu-btn" : "burger-menu-btn"} onClick={() => setHeaderEnabled(!headerEnabled)} />
-                </div>
-            </div>
-
-        </>
-    )
-}
->>>>>>> origin/main
